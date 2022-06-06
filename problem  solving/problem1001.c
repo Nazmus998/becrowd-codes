ec1f55ce3c45ea3562e58028f19573fddc4cc7e9
Nazmus Shakib <105985762+Nazmus998@users.noreply.github.com>
2022-06-06 14:56:46 +0800

    Create problem1001.c

---
+++ problem  solving/problem1001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int A,B;
+    scanf("%d%d",&A,&B);
+    if(B%A ==0)
+    {
+        printf( "sao Multiplos");
+    }
+    else
+    {
+        printf("Nao sao Multiplos");
+    }
+    return 0;
+}
